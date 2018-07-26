2dc918d824eab458a3a8415f8bae03ad18199812
Chi-Hieu <tchieu1995@gmail.com>
2018-07-26 23:03:29 +0700

    Add FlickrPhotosViewController

---
+++ Flickr/FlickrPhotosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPhotosViewController.swift
+//  Flickr
+//
+//  Created by Chi-Hieu on 7/26/18.
+//  Copyright © 2018 Chi-Hieu. All rights reserved.
+//
+
+import UIKit
+
+final class FlickerPhotosViewController : UICollectionViewController {
+    
+    // MARK - Properties
+    fileprivate let reuseIdentifier = "FlickerCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+}
